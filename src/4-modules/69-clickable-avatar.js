7e894c026daee6a303aa3f7661e1d2e7bc3da1f7
Martin Ligabue <martinligabue@gmail.com>
2026-03-15 12:59:49 +0100

    simpler logic
    before accepting please test, it should be the same, just with the logic a bit simplified, but I can't find how to test it locally, the edit in violentmonkey is all one code file

--- src/4-modules/69-clickable-avatar.js
+++ src/4-modules/69-clickable-avatar.js
@@ -10,29 +10,27 @@ function setupClickableAvatar() {
         e.preventDefault()
         e.stopPropagation()
         e.stopImmediatePropagation()
-        if (location.pathname.match(/\/user\/.+\/history/)) {
-            const targetURL = document.querySelector('.dropdown-item[href^="/user/"]').getAttribute("href")
-            if (e.ctrlKey || e.metaKey) {
-                window.open(targetURL, "_blank")
+        const baseURL = document.querySelector('.dropdown-item[href^="/user/"]').getAttribute("href")
+        const isHistoryPage = location.pathname.match(/\/user\/.+\/history/)
+        const targetURL = isHistoryPage ? baseURL : baseURL + "/history"
+        if (targetURL === location.pathname) {
+            miniAvatar.click() // dirty hack for hide dropdown
+            return
+        }
+        if (e.ctrlKey || e.metaKey) {
+            window.open(targetURL, "_blank")
+        } else {
+            if (!isHistoryPage) {
+                try {
+                    getWindow().OSM.router.route(targetURL)
+                } catch {
+                    window.location.pathname = targetURL
+                }
             } else {
                 window.location.pathname = targetURL
             }
-            miniAvatar.click() // dirty hack for hide dropdown
-        } else {
-            const targetURL = document.querySelector('.dropdown-item[href^="/user/"]').getAttribute("href") + "/history"
-            if (targetURL !== location.pathname) {
-                if (e.ctrlKey || e.metaKey) {
-                    window.open(targetURL, "_blank")
-                } else {
-                    try {
-                        getWindow().OSM.router.route(targetURL)
-                    } catch {
-                        window.location.pathname = targetURL
-                    }
-                }
-                miniAvatar.click() // dirty hack for hide dropdown
-            }
         }
+        miniAvatar.click() // dirty hack for hide dropdown
     }
 }
 
